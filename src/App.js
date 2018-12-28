a21797d4571562b18244ebb4fc70f38526daa53d
Keegan Burkett <keegan.t.burkett@gmail.com>
2018-12-27 19:29:43 -0600

    Adds react tilt package

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import DimensionsContainer from "./components/DimensionsContainer";
 import Nav from "./components/Nav";
 import { GlobalStyle } from "./components/Styles";
 
-
 const App = () => (
   <>
     <GlobalStyle />
